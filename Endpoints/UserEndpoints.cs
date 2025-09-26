a68f63e6e9325f5559859b5728e1e73491449eb2
Pascal Perrot <pacofustier@gmail.com>
2025-09-26 19:38:03 +0200

    V2 | Grouping endpoints

---
+++ Endpoints/UserEndpoints.cs
@@ -0,0 +1,114 @@
+﻿using ApiLabo.Dto;
+using ApiLabo.Services;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLabo.Endpoints;
+
+public static class UserEndpoints
+{
+    public static IServiceCollection MapUserServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, EFCoreUserService>();
+        return services;
+    }
+    public static RouteGroupBuilder MapUserEndpoints(this RouteGroupBuilder group)
+    {
+        // GET user
+        group.MapGet("", GetAll)
+            .WithTags("UserManagement")
+            .WithName("GetAll")
+            .Produces(200);
+
+        // GET user/1
+        group.MapGet("{id:int}", GetById)
+            .WithTags("UserManagement")
+            .WithName("GetById")
+            .Produces(200)
+            .Produces(404);
+
+        // DELETE user/1
+        group.MapDelete("{id:int}", Delete)
+            .WithTags("UserManagement")
+            .WithName("Delete")
+            .Produces(204)
+            .Produces(404);
+
+        // PUT user/1
+        group.MapPut("{id:int}", Update)
+            .WithTags("UserManagement")
+            .WithName("Update")
+            .Produces(204)
+            .Produces(404);
+
+        // POST user
+        group.MapPost("", Create)
+            .WithTags("UserManagement")
+            .WithName("Create")
+            .Accepts<UserInputModel>(contentType: "application/json")
+            .Produces<UserOutputModel>(contentType: "application/json")
+            .Produces(200)
+            .Produces(404);
+        return group;
+    }
+    private static async Task<IResult> GetAll(
+                [FromServices] IUserService service,
+                [FromServices] ILogger<Program> logger)
+    {
+        var users = await service.GetAll();
+        logger.LogInformation("Get {count} users", users.Count);
+        return Results.Ok(users);
+    }
+
+    private static async Task<IResult> GetById(
+            [FromRoute] int id,
+            [FromServices] IUserService service)
+    {
+        var userOutput = await service.GetById (id);
+        if (userOutput is null) return Results.NotFound();
+        return Results.Ok(userOutput);
+    }
+
+    private static async Task<IResult> Delete(
+        [FromRoute] int id,
+        [FromServices] IUserService service)
+    {
+        var result = await service.Delete(id);
+        if (result) return Results.NoContent();
+        return Results.NotFound();
+    }
+
+    private static async Task<IResult> Update(
+        [FromRoute] int id,
+        [FromBody] UserInputModel userInput,
+        [FromServices] IUserService service)
+    {
+        var result = await service.Update(id, userInput);
+        if (result) return Results.NoContent();
+        return Results.NotFound();
+    }
+
+    private static async Task<IResult> Create(
+        [FromBody] UserInputModel newUserInput,
+        [FromServices] IValidator<UserInputModel> validator,
+        [FromServices] IUserService service,
+        [FromServices] LinkGenerator linkGenerator,
+        HttpContext httpContext)
+    {
+        var result = validator.Validate(newUserInput);
+        if (!result.IsValid)
+        {
+            var errors = result.Errors.Select(e => new
+            {
+                e.ErrorMessage,
+                e.PropertyName
+            });
+            return Results.BadRequest(errors);
+        }
+        var dbResult = await service.Add(newUserInput);
+        //var link = linkGenerator.GetPathByName("GetById", new { id = dbResult.Id });
+        var link = linkGenerator.GetUriByName(httpContext, "GetById", new { id = dbResult.Id });
+        return Results.Created(link, dbResult);
+    }
+};
+
--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
 ﻿using ApiLabo.Data;
-using ApiLabo.Data.Models;
-using ApiLabo.Dto;
-using ApiLabo.Services;
+using ApiLabo.Endpoints;
 using FluentValidation;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApiDbContext>(opt => opt.UseSqlServer(
     builder.Configuration.GetConnectionString("SqlServer")) 
 );
 
-builder.Services.AddScoped<IUserService, EFCoreUserService>();
+builder.Services.MapUserServices();
 
 builder.Logging.ClearProviders();
 var loggerConfiguration = new LoggerConfiguration()
@@ -26,68 +26,18 @@ builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+    app.UseSwagger();
+}
+
 await app.Services
     .CreateScope().ServiceProvider
     .GetRequiredService<ApiDbContext>().Database
-    //.EnsureCreated();
     .MigrateAsync();
 
-// -- ENDPOINTS
-app.MapGet("/users", async (
-    [FromServices] IUserService service,
-    [FromServices] ILogger<Program> logger) =>
-    {
-        var users = await service.GetAll();
-        logger.LogInformation("Get {count} users", users.Count);
-        return Results.Ok(users);
-    });
-
-app.MapGet("/users/{id:int}", async (
-    [FromRoute] int id,
-    [FromServices] IUserService service) =>
-    {
-        var userOutput = await service.GetById(id);
-        if (userOutput is null) return Results.NotFound();
-        return Results.Ok(userOutput);
-    });
-
-app.MapDelete("/users/{id:int}", async (
-    [FromRoute] int id,
-    [FromServices] IUserService service) =>
-    {
-        var result = await service.Delete(id);
-        if (result) return Results.NoContent();
-        return Results.NotFound();
-    });
-
-app.MapPut("/users/{id:int}", async (
-    [FromRoute] int id,
-    [FromBody] UserInputModel userInput,
-    [FromServices] IUserService service) =>
-    {
-        var result = await service.Update(id, userInput);
-        if (result) return Results.NoContent();
-        return Results.NotFound();
-    });
-
-app.MapPost("/users", async (
-    [FromBody] UserInputModel newUserInput,
-    [FromServices] IValidator<UserInputModel> validator,
-    [FromServices] IUserService service) =>
-    {
-        var result = validator.Validate(newUserInput);
-        if (!result.IsValid)
-        {
-            var errors = result.Errors.Select(e => new
-            {
-                e.ErrorMessage,
-                e.PropertyName
-            });
-
-            return Results.BadRequest(errors);
-        }
-        await service.Add(newUserInput);
-        return Results.Ok(newUserInput);
-    });
+app.MapGroup("/user")
+    .MapUserEndpoints();
 
 app.Run();
